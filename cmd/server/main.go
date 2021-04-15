f579ad9961f12b2141c35a5425598d0c76173d2e
Maria Malysheva <mn.malysheva@mail.ru>
2021-04-15 14:26:00 +0300

    Fix main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"github.com/mp-hl-2021/muzio/internal/interface/httpapi"
+	"github.com/mp-hl-2021/muzio/internal/interface/memory/accountrepo"
+	"github.com/mp-hl-2021/muzio/internal/interface/memory/entityrepo"
+	"github.com/mp-hl-2021/muzio/internal/interface/memory/playlistrepo"
+	"github.com/mp-hl-2021/muzio/internal/usecases/account"
+	"github.com/mp-hl-2021/muzio/internal/usecases/entity"
+	"github.com/mp-hl-2021/muzio/internal/usecases/playlist"
 	"net/http"
 	"time"
 )
 
 func main() {
-	service := httpapi.NewApi()
+	accountUseCases := &account.UseCases{
+		AccountStorage: accountrepo.NewMemory(),
+	}
+	entityUseCases := &entity.UseCases{
+		EntityStorage: entityrepo.NewMemory(),
+	}
+	playlistUseCases := &playlist.UseCases{
+		PlaylistStorage: playlistrepo.NewMemory(),
+	}
+
+	service := httpapi.NewApi(accountUseCases, entityUseCases, playlistUseCases)
 
 	server := http.Server{
 		Addr:         "localhost:8080",
