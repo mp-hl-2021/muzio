e8ae8879977b86b0b437268fbb9a216ee40fb8a4
Maria Malysheva <mn.malysheva@mail.ru>
2021-04-19 12:55:52 +0300

    Use token

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"flag"
 	"github.com/mp-hl-2021/muzio/internal/interface/httpapi"
 	"github.com/mp-hl-2021/muzio/internal/interface/memory/accountrepo"
 	"github.com/mp-hl-2021/muzio/internal/interface/memory/entityrepo"
 	"github.com/mp-hl-2021/muzio/internal/interface/memory/playlistrepo"
+	"github.com/mp-hl-2021/muzio/internal/service/auth"
 	"github.com/mp-hl-2021/muzio/internal/usecases/account"
 	"github.com/mp-hl-2021/muzio/internal/usecases/entity"
 	"github.com/mp-hl-2021/muzio/internal/usecases/playlist"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
 
 func main() {
+	privateKeyPath := flag.String("privateKey", "app.rsa", "file path")
+	publicKeyPath := flag.String("publicKey", "app.rsa.pub", "file path")
+	flag.Parse()
+
+	privateKeyBytes, err := ioutil.ReadFile(*privateKeyPath)
+	publicKeyBytes, err := ioutil.ReadFile(*publicKeyPath)
+
+	a, err := auth.NewJwt(privateKeyBytes, publicKeyBytes, 100*time.Minute)
+	if err != nil {
+		panic(err)
+	}
+
 	accountUseCases := &account.UseCases{
 		AccountStorage: accountrepo.NewMemory(),
+		AuthToken: a,
 	}
 	entityUseCases := &entity.UseCases{
 		EntityStorage: entityrepo.NewMemory(),
@@ -32,7 +48,7 @@ func main() {
 
 		Handler: service.Router(),
 	}
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		panic(err)
 	}
